6177e5230f0ef776eebb76f87949ed9c91a0c1d8
DeAundre Prescott <deaundre.prescott@codeimmersives.com>
2020-08-13 10:09:20 -0400

    done

---
+++ main.js
@@ -0,0 +1,12 @@
+//capOpposite('I am a StInG') //i AM A sTiNg
+let string = 'I am a StInG'
+function capOpposite(str) {
+let result = ''
+for(let i = 0; i > str.length ; i++){
+    if (str[i]){
+        return str[i].upperCase;
+    }
+}
+}
+
+//capOpposite('I am a StInG') //i AM A sTiNg
